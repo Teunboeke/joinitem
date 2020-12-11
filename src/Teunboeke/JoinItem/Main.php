1fcbc96b64ba4257a2c4148e2e7b841e6e9d7184
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-12-11 15:41:38 +0100

    Update Main.php

--- src/Teunboeke/JoinItem/Main.php
+++ src/Teunboeke/JoinItem/Main.php
@@ -2,8 +2,48 @@
 
 namespace Teunboeke\JoinItem;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
+use pocketmine\command\command;
+use pocketmine\command\CommandSender;
+use pocketmine\server;
+use pocketmine\player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\item\Item;
+
+class main extends PluginBase implements Listener{
+
+public function onEnable(){
+        $this->getserver()->getPluginManager()->registerEvents($this, $this);
+$this);       
+        @mkdir($this->getDataFolder();
+        $this->saveResource("config.yml");
+        $this->seveDefaultConfig();
+}
+
+public function onDisable(){
+  $this->getlogger()->info("ItemJoin by Teunboeke has been deactivated
+}
+
+public function onJoin(PlayerJoinEvent $event){
+
+$player = $event->getPlayer();
+$name = $player->getname();
+
+if($this->getConfig()->exists($name)){
+         $this->getConfig()set($name, 1);
+         $this->getConfig()->save();
+}else{
+         $this->getConfig()->set($name, 0);
+         $this->getConfig()->save();
+         
+         $item = Item::get(1, 0, 1);
+         $item->setCustomName("§aSkyBlockUI");
+         $player->getInventory()->setItem(0, $item);
+                  }
+            }
+    }
+
+
+
+
